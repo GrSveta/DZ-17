4db38ca6007aab394cd06cadd9204e0bcc077f87
Sveta Grinkevich <svetl1kgrin@gmail.com>
2022-02-10 14:00:40 +0300

    sc

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const x = prompt("Enter a number"); 
 
 function* factorial(x) {
-  let endfact = 1;
-  for (i=1; i <= x; i++) {
+  const bi = BigInt(x)
+  let endfact = 1n;
+  for (i=1n; i <= bi; i++) {
     endfact *=i;
   }
   yield endfact;
